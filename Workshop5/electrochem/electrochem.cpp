2773f0a8474cf86224617f809dc77973048f08ed
Trevor Keller <trevor.keller@nist.gov>
2017-07-24 14:12:03 -0400

    electrochem parameters and grids

--- Workshop5/electrochem/electrochem.cpp
+++ Workshop5/electrochem/electrochem.cpp
@@ -11,28 +11,38 @@
 
 namespace MMSP {
 
+/*
+	Field 0 is composition.
+	Field 1 is electrostatic potential.
+*/
+
 template <int dim,typename T>
-double Helmholtz(const grid<dim,T>& GRID)
+double Helmholtz(const grid<dim,vector<T> >& GRID)
 {
 	double dV = 1.0;
 	for (int d=0; d<dim; d++)
 		dV *= dx(GRID, d);
 
-	double f = 0.0;
-	double g = 0.0;
+	double fchem = 0.0;
+	double fgrad = 0.0;
+	double felec = 0.0;
 
 	for (int n=0; n<nodes(GRID); n++) {
 		vector<int> x = position(GRID, n);
-		vector<double> gradc = gradient(GRID, x);
-		f += chemenergy(GRID(n));
-		g += gradc*gradc;
+		vector<double> gradc = gradient(GRID, x, 0);
+
+		fchem += chemenergy(GRID(n)[0]);
+		fgrad += gradc*gradc;
+		felec += elecenergy(GRID(n)[0], GRID(n)[1]);
 	}
 
-	double F = dV*(f + 0.5*kappa*g);
+	double F = dV*(fchem + 0.5*kappa*fgrad + felec); // Equation 5
+
 	#ifdef MPI_VERSION
 	double myF(F);
 	MPI::COMM_WORLD.Allreduce(&myF, &F, 1, MPI_DOUBLE, MPI_SUM);
 	#endif
+
 	return F;
 }
 
@@ -44,32 +54,31 @@ void generate(int dim, const char* filename)
 	rank = MPI::COMM_WORLD.Get_rank();
 	#endif
 
-	if (dim!=2 && rank==0) {
+	if (dim != 2 && rank == 0) {
 		std::cerr<<"ERROR: CHiMaD problems are 2-D, only!"<<std::endl;
 		std::exit(-1);
 	}
 
 	if (dim==2) {
-		GRID2D initGrid(1,0,200,0,200);
+		GRID2D initGrid(2,0,200,0,200);
 		for (int d=0; d<dim; d++)
 			dx(initGrid,d) = deltaX;
 
 		for (int n=0; n<nodes(initGrid); n++) {
 			vector<int> x = position(initGrid, n);
-			initGrid(n) = cheminit(dx(initGrid,0)*x[0], dx(initGrid,1)*x[1]);
+			initGrid(n)[0] = cheminit(dx(initGrid,0)*x[0], dx(initGrid,1)*x[1]);
+			initGrid(n)[1] = 0.0;
 		}
 
-		ghostswap(initGrid);
 		output(initGrid,filename);
-
 	}
 }
 
 template <int dim, typename T>
-void update(grid<dim,T>& oldGrid, int steps)
+void update(grid<dim,vector<T> >& oldGrid, int steps)
 {
-	grid<dim,T> newGrid(oldGrid);
-	grid<dim,T> lapGrid(oldGrid);
+	grid<dim,vector<T> > newGrid(oldGrid);
+	grid<dim,T> lapGrid(oldGrid, 0); // scalar grid from vector grid: zero fields
 	// Make sure the grid spacing is correct
 	for (int d=0; d<dim; d++) {
 		dx(oldGrid,d) = deltaX;
@@ -82,17 +91,23 @@ void update(grid<dim,T>& oldGrid, int steps)
 
 		for (int n=0; n<nodes(oldGrid); n++) {
 			vector<int> x = position(oldGrid, n);
-			const T& c = oldGrid(n);
-			lapGrid(n) = dfdc(c) - kappa*laplacian(oldGrid, x);
+			const T& c = oldGrid(n)[0];
+			const T& p = oldGrid(n)[1];
+			lapGrid(n) = dfdc(c) - kappa*laplacian(oldGrid, x, 0) + k*p;
 		}
 
 		ghostswap(lapGrid);
 
+		// Apply Equation of Motion for composition (forward Euler)
 		for (int n=0; n<nodes(oldGrid); n++) {
 			vector<int> x = position(oldGrid, n);
-			newGrid(n) = oldGrid(n) + dt*M*laplacian(lapGrid, x);
+			newGrid(n)[0] = oldGrid(n)[0] + dt*M*laplacian(lapGrid, x);
 		}
 
+		// Solve Poisson Equation for electrostatic potential (Gauss-Seidel)
+
+
+		// ~ fin ~
 		swap(oldGrid,newGrid);
 	}
 	ghostswap(oldGrid);
--- Workshop5/electrochem/energy.hpp
+++ Workshop5/electrochem/energy.hpp
@@ -6,23 +6,38 @@
 #define CAHNHILLIARD_ENERGY
 #include<cmath>
 
-const double deltaX = 1.0;
-const double Ca = 0.3;
-const double Cb = 0.7;
-const double rhoS = 5.0;
+// Composition parameters
+const double Ca = 0.30; // alpha composition
+const double Cb = 0.70; // beta composition
+const double C0 = 0.50; // system composition
+const double C1 = 0.04; // fluctuation magnitude
+
+// Electrostatic parameters
+const double k = 0.09;
+const double epsilon = 9.0; // permittivity
+
+// Physical parameters
+const double rho = 5.0;
 const double kappa = 2.0;
 const double M = 5.0;
-const double C0 = 0.5;
-const double epsilon = 0.01;
-const double CFL = 0.25;
+
+// Numerical parameters
+const double CFL = 0.20;
+const double deltaX = 1.0;
 const double dt = std::pow(deltaX, 4)*CFL/(24.0*M*kappa);
 
 double chemenergy(const double& C)
 {
-	// Equation 2
+	// Equation 6
 	const double A = C-Ca;
 	const double B = Cb-C;
-	return rhoS * A*A * B*B;
+	return rho * A*A * B*B;
+}
+
+double elecenergy(const double& C, const double& P)
+{
+	// Equation 7
+	return 0.5 * k * C * P;
 }
 
 double dfdc(const double& C)
@@ -30,16 +45,15 @@ double dfdc(const double& C)
 	// d(chemenergy)/dc
 	const double A = C-Ca;
 	const double B = Cb-C;
-	return 2.0 * rhoS * A * B * (Ca + Cb - 2.0 * C);
+	return 2.0 * rho * A * B * (Ca + Cb - 2.0 * C);
 }
 
 double cheminit(const double& x, const double& y)
 {
 	// Equation 12
-	return C0 + epsilon * ( std::cos(0.105*x)          * std::cos(0.11*y)
-	                      + std::pow(std::cos(0.13*x)  * std::cos(0.087*y), 2.0)
-	                      + std::cos(0.025*x - 0.15*y) * std::cos(0.07*x - 0.02*y)
-                          );
+	return C0 + C1 * ( std::cos(0.200*x)          * std::cos(0.11*y)
+	                 + std::pow(std::cos(0.13*x)  * std::cos(0.087*y), 2.0)
+	                 + std::cos(0.025*x - 0.15*y) * std::cos(0.07*x - 0.02*y));
 }
 
 #endif
